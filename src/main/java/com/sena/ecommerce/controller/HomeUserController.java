13c1bab7c4af4d8c5732b8fe81f8fcecac8b4eee
unknown <heady_melo@soy.sena.edu.co>
2024-11-19 14:59:26 -0500

    v0.9_vistaProductoHomeMetodoProductoHomeEnHomeUserController_19112024_Nan

--- src/main/java/com/sena/ecommerce/controller/HomeUserController.java
+++ src/main/java/com/sena/ecommerce/controller/HomeUserController.java
@@ -1,23 +1,49 @@
 package com.sena.ecommerce.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.service.annotation.PatchExchange;
 
+import com.sena.ecommerce.model.Producto;
 import com.sena.ecommerce.service.IProductoService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/") // la raiz del proyecto
 public class HomeUserController {
-	
+
+	// instancia del LOGGER para ver datos por consola
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(HomeUserController.class);
+
+	// instancia de objeto - servicio
 	@Autowired
 	private IProductoService productoService;
 
+	// metodo que mapea la vista de usuario en la raiz del proyecto
 	@GetMapping("")
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+
+	// metodo que carga el producto del usuario con el id
+	@GetMapping("productoHome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		LOGGER.info("ID producto enviado como parametro {}", id);
+		// variable de clase producto
+		Producto p = new Producto();
+		// objeto de tipo optional
+		Optional<Producto> op = productoService.get(id);
+		p = op.get();
+		// enviar a la vista con el model los detalles del producto con el id
+		model.addAttribute("producto", p);
+		return "usuario/productoHome";
 	}
 }
