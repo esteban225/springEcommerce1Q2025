e6ac301df28c4b657412bb3c08e5166999420e67
estebanRicardo <esteban_ricardo@soy.sena.edu.co>
2025-02-11 14:17:39 -0500

    v0.13_vitaRegistroUsuarios&UsuarioController_11022025_teban

---
+++ src/main/java/com/sena/ecommerce/controller/UsuarioConreoller.java
@@ -0,0 +1,36 @@
+package com.sena.ecommerce.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sena.ecommerce.model.Usuario;
+import com.sena.ecommerce.service.IUsuarioService;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioConreoller {
+	
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(UsuarioConreoller.class);
+
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	@GetMapping("/registro")
+	public String createUser() {
+		return "usuario/registro";
+	}
+
+	@PostMapping("/save")
+	public String save(Usuario usuario, Model model) {
+		LOGGER.info("Usuario a registrar: {}", usuario);
+		usuario.setTipo("USER");
+		usuarioService.save(usuario);
+		return "redirect:/";
+	}
+}
